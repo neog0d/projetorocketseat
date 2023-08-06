f226cf17a10b9247dcc75e2c3ead8863c059d801
amanda <diasamanda1105@gmail.com>
2023-08-06 16:39:49 -0300

    :cool: initial commit

--- .vscode/script.js
+++ .vscode/script.js
@@ -8,10 +8,10 @@ function toggleMode() {
   // substituir a imagem
   if(html.classList.contains('light')) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/assets/avatar-light.png")
+    img.setAttribute("src", "./assets/assets/avatar-simples.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/assets/avatar.png")
+    img.setAttribute("src", "./assets/assets/avatar-simples.png")
   }
 }
 
